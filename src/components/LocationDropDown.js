25c3401611594e1ac8c6fd98b40f0c27bc84865b
Kit <kit.jrs@gmail.com>
2019-07-26 14:57:22 +0700

    กำหนดการเลือกข้อมูลจาก dropdown

--- src/components/LocationDropDown.js
+++ src/components/LocationDropDown.js
@@ -1,32 +1,40 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class LocationDropDown extends Component {
-  render() {
 
-    let locations = this.props.locations;
-    console.log(locations);
+    locationSelected(e){
+        console.log(e.target);
+        let locationName = e.target.getAttribute('data-name');
+        console.log(locationName);
+        
+    }
 
-    return (
-      <div className="dropdown">
-        <a
-          className="btn btn-secondary dropdown-toggle"
-          href="#"
-          role="button"
-          id="dropdownMenuLink"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false">
-          Dropdown link
-        </a>
+    render() {
 
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          {
-              locations.map((location, i) => 
-                <a className="dropdown-item" href="#" key={location.id} data-id={location.id} data-name={location.name}>{location.name}</a>
-              )
-          }
-        </div>
-      </div>
-    )
-  }
+        let locations = this.props.locations;
+        let defaultLabel = this.props.defaultLabel;
+
+        return (
+            <div className="dropdown">
+                <a
+                    className="btn btn-secondary dropdown-toggle"
+                    href="#"
+                    role="button"
+                    id="dropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false">
+                    {defaultLabel}
+                </a>
+
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    {
+                        locations.map((location, i) =>
+                            <a className="dropdown-item" href="#" onClick={e => this.locationSelected(e)} key={location.id} data-id={location.id} data-name={location.name}>{location.name}</a>
+                        )
+                    }
+                </div>
+            </div>
+        )
+    }
 }
